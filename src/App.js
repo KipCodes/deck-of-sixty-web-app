cb2fc3c04367711269093c409369054b87fb6801
Kelly Parrish <kparrish1814@gmail.com>
2023-01-06 19:56:11 -0600

    removed files and folders, starting from scratch and reverting to an EJS model instead of MERN

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import Header from './components/Header'
-
-function App () {
-    return (
-        <h1>testing testing testing</h1>
-    );
-}
-
-export default App;
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header>
-        <h1> Final Fantasy XIV's Deck of Sixty</h1>
-    </header>
-  )
-}
-
-export default Header
