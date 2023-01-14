26c669ec4bc69d72ffd7c839c9754894e3321ee5
Kelly Parrish <kparrish1814@gmail.com>
2023-01-13 22:13:14 -0600

    re-established files with ejs infrastructure and got it connected and working. More to come!

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
-const connectDB = require('./config/database')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
 
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
 
-require('dotenv').config({path: './config/.env'})
+//Connect To Database
+connectDB();
 
-connectDB()
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
