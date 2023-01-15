133b1e017064419de1816cac11a5a2117e405821
Kelly Parrish <kparrish1814@gmail.com>
2023-01-14 22:47:20 -0600

    added some content to the main index page to begin building off of. no working links as of yet.

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
+const threeCardController = require('../controllers/three-card');
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get('/three-card', threeCardController.getThreeCard);
 
 module.exports = router;
